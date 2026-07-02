cd27fb4ac0f187913b28da458724b39f19358d0d
hazratali-create <hazratali201988ullah@gmail.com>
2026-07-02 14:29:17 +0500

    Initialize Express application with a welcome route

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hazrat Ali! Application GitHub se AWS App Runner par kamyabi se chal rahi hai! 🚀</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
